8416f51749c9636eef28d19c68f918bfa22ca642
Valeria Gabrielli <gabvale53@gmail.com>
2026-01-28 00:48:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+const endpoint = ' https://lanciweb.github.io/demo/api/actresses/'
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
