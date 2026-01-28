3139aed834aa392ee490ed4c0de538a8a035a920
Valeria Gabrielli <gabvale53@gmail.com>
2026-01-28 17:46:21 +0100

    feat: imposto useEffect in app.jsx + prima visione in pagina delle card attrici

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
-
 const endpoint = ' https://lanciweb.github.io/demo/api/actresses/'
 function App() {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    axios.get(endpoint)
+      .then((res)=>{
+        console.log(res.data)
+        setList(res.data)
+      })
+  },[])
 
   return (
     <>
-
+  {
+    list.map((actress)=>{
+      return(
+        <>
+        <h2>{actress.name}</h2>
+        <img src={actress.image} alt="" />
+        </>
+      )
+    })
+  }
     </>
   )
 }
